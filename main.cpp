55356de02de379646340b7a18e5a35dfaed053b3
Emilia Masiak <masiak.emilia@o2.pl>
2022-03-17 23:24:18 +0100

    Początki projektu

---
+++ main.cpp
@@ -0,0 +1,38 @@
+﻿//My first game :)
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "MyFunctions.h"
+
+void score();
+void time();
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1200, 800), "Title", sf::Style::Close | sf::Style::Titlebar);
+
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case sf::Event::Closed:
+                window.close();
+                break;
+            case sf::Event::LostFocus:
+                break;
+            case sf::Event::GainedFocus:
+                break;
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
