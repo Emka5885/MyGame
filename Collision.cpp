2594188d75437d83ff8261de1b5333648ead507d
Emilia Masiak <masiak.emilia@o2.pl>
2022-04-01 21:49:25 +0200

    Przemyślenia związane z "Collision.h" (podstawowe deklaracje)

--- Collision.cpp
+++ Collision.cpp
@@ -1,1 +1,15 @@
 #include "Collision.h"
+
+Collision::Collision(sf::CircleShape& cir1)
+	: cir1(cir1)
+{
+}
+
+Collision::~Collision()
+{
+}
+
+bool Collision::checkCollision(sf::CircleShape& cir2)
+{
+	return false;
+}
--- Collision.h
+++ Collision.h
@@ -1,5 +1,15 @@
 #pragma once
+#include <SFML\Graphics.hpp>
+#include <iostream>
+
 class Collision
 {
-};
+private:
+	sf::CircleShape& cir1;
 
+public:
+	Collision(sf::CircleShape& cir1);
+	~Collision();
+
+	bool checkCollision(sf::CircleShape& cir2);
+};
