b96f25345bd501a82ca25b1c85858b36aab95325
Emka5885 <90275335+Emka5885@users.noreply.github.com>
2022-04-10 22:56:57 +0200

    Wstępne prace nad użycia joysticka (Player.h)

--- Player.h
+++ Player.h
@@ -8,14 +8,18 @@ class Player
 private:
 	sf::Cursor c1;
 	sf::Image* image;
+	sf::Texture* texture;
 	sf::CircleShape* b;
 	int vibrations;
 	int ammunition;
 	int score1;
 	int tv;
+	bool mouse;
+
+	sf::RectangleShape rec;
 
 public:
-	Player(sf::Image* image, int vibrations, float ammunition, sf::RenderWindow& win, int tv);
+	Player(sf::Image* image, sf::Texture* texture, int vibrations, float ammunition, sf::RenderWindow& win, int tv, bool mouse);
 	~Player();
 
 	void create(sf::RenderWindow& win);
@@ -26,4 +30,6 @@ public:
 	int& getAmmunition();
 	int& getVibrations();
 	int getTimeV();
+	bool getMouse();
+	sf::RectangleShape& getRec();
 };
